f07e00a7dde692df6d0a9c460952fb7ba4059616
dj-lumiere <djminstar573@gmail.com>
2025-08-09 14:25:37 +0900

    feat: added ToReprTree support

--- src/Runtime/Repr/Formatters/Unity/UnityColorFormatters.cs
+++ src/Runtime/Repr/Formatters/Unity/UnityColorFormatters.cs
@@ -1,29 +1,70 @@
 ﻿using DebugUtils.Unity.Repr.Attributes;
 using DebugUtils.Unity.Repr.Interfaces;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace DebugUtils.Unity.Repr.Formatters
 {
     [ReprFormatter(typeof(Color))]
     [ReprOptions(needsPrefix: true)]
-    internal class ColorFormatter : IReprFormatter
+    internal class ColorFormatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Color)obj;
-            return
-                $"R{(int)(t.r * 255)} G{(int)(t.g * 255)} B{(int)(t.b * 255)} A{(int)(t.a * 255)}";
+            return $"R{t.r * 100:F0}% G{t.g * 100:F0}% B{t.b * 100:F0}% A{t.a * 100:F0}%";
+        }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Color)obj;
+            Color.RGBToHSV(rgbColor: t, H: out var h, S: out var s, V: out var v);
+            return new JObject
+            {
+                [propertyName: "type"] = "Color",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "r"] = t.r.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "g"] = t.g.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "b"] = t.b.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "a"] = t.a.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "h"] = h.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "s"] = s.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "v"] = v.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
         }
     }
 
     [ReprFormatter(typeof(Color32))]
     [ReprOptions(needsPrefix: true)]
-    internal class Color32Formatter : IReprFormatter
+    internal class Color32Formatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Color32)obj;
             return $"R{t.r:X2} G{t.g:X2} B{t.b:X2} A{t.a:X2}";
+        }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Color32)obj;
+            Color.RGBToHSV(rgbColor: t, H: out var h, S: out var s, V: out var v);
+            var hByte = (byte)(h * 255);
+            var sByte = (byte)(s * 255);
+            var vByte = (byte)(v * 255);
+            return new JObject
+            {
+                [propertyName: "type"] = "Color",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "r"] = t.r.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "g"] = t.g.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "b"] = t.b.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "a"] = t.a.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "h"] =
+                    hByte.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "s"] =
+                    sByte.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "v"] = vByte.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
         }
     }
 }
--- src/Runtime/Repr/Formatters/Unity/UnityVectorFormatters.cs
+++ src/Runtime/Repr/Formatters/Unity/UnityVectorFormatters.cs
@@ -1,50 +1,118 @@
 ﻿using DebugUtils.Unity.Repr.Attributes;
 using DebugUtils.Unity.Repr.Interfaces;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace DebugUtils.Unity.Repr.Formatters
 {
     [ReprFormatter(typeof(Vector2))]
     [ReprOptions(needsPrefix: true)]
-    internal class Vector2Formatter : IReprFormatter
+    internal class Vector2Formatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Vector2)obj;
             return $"{t.x:F2},{t.y:F2}";
         }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Vector2)obj;
+            return new JObject
+            {
+                [propertyName: "type"] = "Vector2",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "x"] = t.x.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "y"] = t.y.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "magnitude"] =
+                    t.magnitude.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "normalized"] =
+                    t.normalized.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
+        }
     }
 
     [ReprFormatter(typeof(Vector3))]
     [ReprOptions(needsPrefix: true)]
-    internal class Vector3Formatter : IReprFormatter
+    internal class Vector3Formatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Vector3)obj;
             return $"{t.x:F2},{t.y:F2},{t.z:F2}";
         }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Vector3)obj;
+            return new JObject
+            {
+                [propertyName: "type"] = "Vector3",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "x"] = t.x.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "y"] = t.y.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "z"] = t.z.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "magnitude"] =
+                    t.magnitude.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "normalized"] =
+                    t.normalized.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
+        }
     }
 
     [ReprFormatter(typeof(Vector4))]
     [ReprOptions(needsPrefix: true)]
-    internal class Vector4Formatter : IReprFormatter
+    internal class Vector4Formatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Vector4)obj;
             return $"{t.x:F2},{t.y:F2},{t.z:F2},{t.w:F2}";
         }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Vector4)obj;
+            return new JObject
+            {
+                [propertyName: "type"] = "Vector4",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "x"] = t.x.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "y"] = t.y.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "z"] = t.z.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "w"] = t.w.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "magnitude"] =
+                    t.magnitude.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "normalized"] =
+                    t.normalized.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
+        }
     }
 
     [ReprFormatter(typeof(Quaternion))]
     [ReprOptions(needsPrefix: true)]
-    internal class QuaternionFormatter : IReprFormatter
+    internal class QuaternionFormatter : IReprFormatter, IReprTreeFormatter
     {
         public string ToRepr(object obj, ReprContext context)
         {
             var t = (Quaternion)obj;
             return $"{t.x:F2},{t.y:F2},{t.z:F2},{t.w:F2}";
+        }
+
+        public JToken ToReprTree(object obj, ReprContext context)
+        {
+            var t = (Quaternion)obj;
+            return new JObject
+            {
+                [propertyName: "type"] = "Vector4",
+                [propertyName: "kind"] = "struct",
+                [propertyName: "x"] = t.x.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "y"] = t.y.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "z"] = t.z.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "w"] = t.w.FormatAsJToken(context: context.WithIncrementedDepth()),
+                [propertyName: "eulerAngles"] =
+                    t.eulerAngles.FormatAsJToken(context: context.WithIncrementedDepth())
+            };
         }
     }
 }
